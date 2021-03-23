8c5b00e8da7dd022ce809e54df80ed24912547e0
Aparna <mail4aparna13@gmail.com>
2021-03-23 21:05:05 +0530

    Commit 7

--- CO1/Program 7.py
+++ CO1/Program 7.py
@@ -9,13 +9,13 @@ if sum(list1)==sum(list2):
     print('->Sums of list elements are equal')
 else:
     print('->Sums of list elements are not equal')
-flag=0
+f=0
 for i in list1:
     for j in list2:
         if i==j:
-            flag+=1
-            print(i)
-if flag==0:
-    print('->No common elements in both list')
+            f+=1
+            print(i,end=',')
+if(f==0):
+    print("No common elements")
 else:
-    print('is in both list')
+    print("is in both list")
