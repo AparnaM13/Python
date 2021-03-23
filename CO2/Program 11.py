2ff305824d611e6ddb0f569f5266d8623783515e
Aparna <mail4aparna13@gmail.com>
2021-03-23 21:43:11 +0530

    Commit 11

--- CO2/Program 11.py
+++ CO2/Program 11.py
@@ -1,14 +1,9 @@
 #Write lambda functions to find area of square,rectangle and triangle
-a=int(input("Enter first side of triangle: "))
-b=int(input("Enter second side of triangle: "))
-c=int(input("Enter third side of triangle: "))
-s=(a+b+c)/2
-area=(s*(s-a)*(s-b)*(s-c))**0.5
-print("Area of triangle is : ",area)
-a=int(input("Enter the side of square: "))
-area=a*a
-print("Area of square is: ",area)
-a=int(input("Enter length of rectangle: "))
-b=int(input("Enter breadth of rectengle: "))
-area=a*b
-print("Area of rectangle is: ",area)
+square = lambda a: a * a
+print("Area of the square is", square(10))
+
+rectangle = lambda a, b: a * b
+print("Area of the rectangle is", rectangle(10, 5))
+
+triangle = lambda b, h: (b * h) / 2
+print("Area of the rectangle is", triangle(4, 2))
